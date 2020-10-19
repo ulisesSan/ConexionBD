419299456baa5b881fba5d8d66f0d8fce8fd7614
ulisesSan <ulisesSan@users.noreply.github.com>
2020-10-19 10:47:00 -0500

    First Commit

---
+++ Facturacion2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Facturacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CTR_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CTR_btn
+            // 
+            this.CTR_btn.Location = new System.Drawing.Point(171, 150);
+            this.CTR_btn.Name = "CTR_btn";
+            this.CTR_btn.Size = new System.Drawing.Size(131, 43);
+            this.CTR_btn.TabIndex = 0;
+            this.CTR_btn.Text = "Conexion";
+            this.CTR_btn.UseVisualStyleBackColor = true;
+            this.CTR_btn.Click += new System.EventHandler(this.CTR_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 256);
+            this.Controls.Add(this.CTR_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CTR_btn;
+    }
+}
+
---
+++ Facturacion2/conexion2.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Facturacion2
+{
+    class conexion2
+    {
+        public static void conexion()
+        {
+            string servidor = "localhost"; //Nombre o ip del servidor de MySQL
+            string bd = "mysql"; //Nombre de la base de datos
+            string usuario = "ulises"; //Usuario de acceso a MySQL
+            string password = "a"; //Contraseña de usuario de acceso a MySQL
+            string datos = null; //Variable para almacenar el resultado
+
+            //Crearemos la cadena de conexión concatenando las variables
+            string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User Id=" + usuario + "; Password=" + password + "";
+
+            //Instancia para conexión a MySQL, recibe la cadena de conexión
+            MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+            MySqlDataReader reader = null; //Variable para leer el resultado de la consulta
+
+            //Agregamos try-catch para capturar posibles errores de conexión o sintaxis.
+
+            try
+            {
+                string consulta = "SHOW DATABASES"; //Consulta a MySQL (Introduce datos a la bd)
+                MySqlCommand comando = new MySqlCommand(consulta); //Declaración SQL para ejecutar contra una base de datos MySQL
+                comando.Connection = conexionBD; //Establece la MySqlConnection utilizada por esta instancia de MySqlCommand
+                conexionBD.Open(); //Abre la conexión
+                reader = comando.ExecuteReader(); //Ejecuta la consulta y crea un MySqlDataReader
+
+                while (reader.Read()) //Avanza MySqlDataReader al siguiente registro
+                {
+                    datos += reader.GetString(0) + "\n"; //Almacena cada registro con un salto de linea
+                }
+
+                MessageBox.Show(datos+"\n Conexion exisosa"); //Imprime en cuadro de dialogo el resultado
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message); //Si existe un error aquí muestra el mensaje
+            }
+            finally
+            {
+                conexionBD.Close(); //Cierra la conexión a MySQL
+            }
+        }
+    }
+}
